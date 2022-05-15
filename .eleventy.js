bd9e6723281da4ee00d5d157346c2f55d112b245
Edward Mason <mynameisprivate3@gmail.com>
2022-05-15 15:46:53 +0100

    Lesson 12: Blog post & Filters

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
+// Filters
+const dateFilter = require('./src/filters/date-filter.js');
+const w3DateFilter = require('./src/filters/w3-date-filter.js');
+
 module.exports = config => {
     config.addPassthroughCopy('./src/images/');
     const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+    // Add filters
+    config.addFilter('dateFilter', dateFilter);
+    config.addFilter('w3DateFilter', w3DateFilter);
     // Returns a collection of blog posts in reverse date order
     config.addCollection('blog', collection => {
       return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
